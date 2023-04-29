9781baded19dac5656ee5ede53eb19aafc2f176c
sriram-sahu <sriramsahu510@gamail.com>
2023-04-29 16:17:41 +0530

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Signup from "../Signup";
+import Login from "../Login";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <button>Login</button>
+        <button>Signup</button>
+      </>
+    );
+  }
+}
+export default Home;
